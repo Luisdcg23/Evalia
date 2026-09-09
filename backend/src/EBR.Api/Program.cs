51fa23f48a9b5089838046216d26181684852422
Luis Angel de la Cruz <luisdlcg23@gmail.com>
2026-09-09 10:05:44 -0400

    test: isolate integration logging from Windows event log

--- backend/src/EBR.Api/Program.cs
+++ backend/src/EBR.Api/Program.cs
@@ -8,9 +8,15 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.Logging;
 using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsEnvironment("Testing"))
+{
+    builder.Logging.ClearProviders();
+}
 
 builder.Services.AddOpenApi();
 builder.Services.AddProblemDetails();
